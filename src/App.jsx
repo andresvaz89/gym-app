1815565942cfe494742dcd3eb4ac0f2a41fc369b
Andres Vazquez <andres.vazquez.martin@gmail.com>
2024-02-19 21:28:24 +0100

    debug responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 import ExerciseDetail from './pages/ExerciseDetail';
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -8,6 +8,14 @@ export const exerciseOptions = {
   }
 };
 
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'a22ff582c1msh3f0289436cafaf3p1b84f9jsn11c68a575e6b'
+  }
+};
+
 export const fetchData = async (url, options) => {
   const response = await fetch(url, options);
   const data = await response.json();
